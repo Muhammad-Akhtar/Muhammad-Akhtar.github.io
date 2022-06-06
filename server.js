391dd525af7e059a3407d1ca365687c336be1e63
Muhammad-Akhtar <akhtarm821@gmail.com>
2022-06-06 13:57:31 +0500

    Latest Code Pushed

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var port = 8001;
+
+// below statement is to execute index.html by default
+// If your html name is different then put your html name (__dirname + '/demo.html')
+app.use(express.static(__dirname + '/'));
+
+// start a serve on given port
+app.listen(port);
+
+console.log("Server started at: " + port);
