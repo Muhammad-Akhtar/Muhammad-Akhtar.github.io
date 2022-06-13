5d17dc2c43fc9ceb4273d0e3d5a7780d5429f66a
mac <noreply@reachme.com>
2022-06-13 07:51:37 +0500

    Experience section of bitsoft finalized

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-$(function(){
-    $(window).on("load", function(){
+$(function () {
+    $(window).on("load", function () {
         $(".fill-screen").css("height", window.innerHeight);
+        var cardInnerHeight = parseInt($('.card > .front').innerHeight()) / 2 - 25;
+        $(".ic-image").css("height", cardInnerHeight);
+
     });
 });
