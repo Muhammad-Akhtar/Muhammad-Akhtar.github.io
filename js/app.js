ea6e3b871fe9425db8bd22037de92d3c87d63a48
mac <noreply@reachme.com>
2022-06-15 08:39:19 +0500

    working on experience section

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ $(function () {
     $(window).on("load", function () {
         $(".fill-screen").css("height", window.innerHeight);
         var cardInnerHeight = parseInt($('.card > .front').innerHeight()) / 2 - 25;
+        // var cardInnerWidth = parseInt($('.card > .front').innerWidth()) / 2 - 25;
         $(".ic-image").css("height", cardInnerHeight);
-
+        // $(".ic-image").css("height", cardInnerWidth);
     });
 });
