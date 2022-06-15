c6c0137a649f5740498692465a44d8553e615f05
Muhammad-Akhtar <akhtarm821@gmail.com>
2022-06-15 10:19:07 +0500

    front of all cards in experience section finalized

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 $(function () {
     $(window).on("load", function () {
         $(".fill-screen").css("height", window.innerHeight);
-        var cardInnerHeight = parseInt($('.card > .front').innerHeight()) / 2 - 25;
-        // var cardInnerWidth = parseInt($('.card > .front').innerWidth()) / 2 - 25;
+        var cardInnerHeight = parseInt($('.card > .front').innerHeight()) / 2 - 50;
         $(".ic-image").css("height", cardInnerHeight);
-        // $(".ic-image").css("height", cardInnerWidth);
     });
 });
