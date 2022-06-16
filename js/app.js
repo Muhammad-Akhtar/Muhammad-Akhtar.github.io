37fa8501d2e90cdb5f10e5a3d7ab99a538c8d0e1
mac <noreply@reachme.com>
2022-06-17 00:01:39 +0500

    Skills bugs fixed

--- js/app.js
+++ js/app.js
@@ -5,3 +5,16 @@ $(function () {
         $(".ic-image").css("height", cardInnerHeight);
     });
 });
+
+$(document).ready(function () {
+    $(".progress-bar").each(function (i, v) {
+        // var bar_value = v.getAttribute('aria-valuenow');
+        var bar_value = $(this).attr('aria-valuenow');
+        $(v).animate({ width: bar_value + "%" }, 4000, 'swing', function (bar_value) {
+            debugger
+            v = $(this).parent().children('.percent').html($(this).attr('aria-valuenow') + '%');
+            // $(v).html(45);
+            // alert($().html('45'));
+        })
+    })
+})
